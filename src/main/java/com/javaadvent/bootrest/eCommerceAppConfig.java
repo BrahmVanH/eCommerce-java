876e0b2d43d5710933dd340058ee985d60c666b7
Brahm V <brahmvanh@gmail.com>
2023-09-12 21:03:43 -0400

    Nuked the eCommerce java app that was using Morphia with Mongo, switched to vs code from intellij, and started a new project using spring-boot with mongo.

---
+++ src/main/java/com/javaadvent/bootrest/eCommerceAppConfig.java
@@ -0,0 +1,16 @@
+package com.javaadvent.bootrest;
+ 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+ 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class eCommerceAppConfig {
+     
+    public static void main(String[] args) {
+        SpringApplication.run(eCommerceAppConfig.class, args);
+    }
+}
---
+++ src/main/java/com/javaadvent/bootrest/model/Product.java
@@ -0,0 +1,12 @@
+package com.javaadvent.bootrest.model;
+
+import org.springframework.data.annotation.Id;
+
+import static com.javaadvent.bootrest.util.PreCondition.isTrue;
+import static com.javaadvent.bootrest.util.PreCondition.notEmpty;
+import static com.javaadvent.bootrest.util.PreCondition.notNull;
+
+
+final class Product {
+  
+}
