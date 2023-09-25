85ae9fec285fd05f2595eed3088b5ab82b65d5f0
Brahm V <brahmvanh@gmail.com>
2023-09-24 20:49:26 -0400

    Finished creating Product entity

--- src/main/java/com/javaadvent/bootrest/model/Product.java
+++ src/main/java/com/javaadvent/bootrest/model/Product.java
@@ -6,10 +6,8 @@ import static com.javaadvent.bootrest.util.PreCondition.isTrue;
 import static com.javaadvent.bootrest.util.PreCondition.notEmpty;
 import static com.javaadvent.bootrest.util.PreCondition.notNull;
 
-
 final class Product {
-  static final int MAX_LENGTH_DESCRIPTION = 500;
-  static final int MAX_LENGTH_TITLE = 100;
+  static final int MAX_LENGTH_NAME = 100;
 
   @Id
   private String id;
@@ -17,7 +15,7 @@ final class Product {
   private String name;
 
   private double price;
-  
+
   private int stock;
 
   private Product(Builder builder) {
@@ -31,7 +29,80 @@ final class Product {
     return new Builder();
   }
 
+  // Getters
 
+  private String getId() {
+    return this.id;
+  }
 
+  private String getName() {
+    return this.name;
+  }
+
+  private double getPrice() {
+    return this.price;
+  }
+
+  private int getStock() {
+    return this.stock;
+  }
+
+  public void update(String name, double price, int stock) {
+    checkValues(name, price, stock);
+
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  static class Builder {
+    private String name;
+    private double price;
+    private int stock;
+
+    private Builder() {
+    };
+
+    Builder name(String name) {
+      this.name = name;
+
+      return this;
+    }
+
+    Builder price(double price) {
+      this.price = price;
+
+      return this;
+    }
+
+    Builder stock(int stock) {
+      this.stock = stock;
+
+      return this;
+    }
+
+    Product build() {
+      Product build = new Product(this);
+
+      build.checkValues(build.getName(), build.getPrice(), build.getStock());
+
+      return build;
+    }
+
+  }
+
+  private void checkValues(String name, double price, int stock) {
+    notNull(name, "The product name cannot be null!");
+    notEmpty(name, "The product name cannot be empty!");
+    isTrue(name.length() <= MAX_LENGTH_NAME,
+        "Title cannot be longer than %d characters", MAX_LENGTH_NAME
+        );
+
+    notNull(price, "The product price cannot be null!");
+    notEmpty(price, "The product price cannot be empty!");
+
+    notNull(stock, "The product stock cannot be null!");
+    notEmpty(stock, "The product stock cannot be empty!");
+  }
 
 }
