414b129addfd2d12085c7c26890ca097c4319e3b
Brahm V <brahmvanh@gmail.com>
2023-09-24 20:29:55 -0400

    Added main class configuration to pom file

--- src/main/java/com/javaadvent/bootrest/model/Product.java
+++ src/main/java/com/javaadvent/bootrest/model/Product.java
@@ -8,5 +8,30 @@ import static com.javaadvent.bootrest.util.PreCondition.notNull;
 
 
 final class Product {
+  static final int MAX_LENGTH_DESCRIPTION = 500;
+  static final int MAX_LENGTH_TITLE = 100;
+
+  @Id
+  private String id;
+
+  private String name;
+
+  private double price;
   
+  private int stock;
+
+  private Product(Builder builder) {
+    this.name = builder.name;
+    this.price = builder.price;
+    this.stock = builder.stock;
+
+  }
+
+  static Builder getBuilder() {
+    return new Builder();
+  }
+
+
+
+
 }
